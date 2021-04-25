d982998b377ea57f14ebd5a45b2f7ad4f60c5dca
Joshua Liang <joshuali@zillowgroup.com>
2021-04-25 11:53:42 -0700

    init

---
+++ src/test/java/percent/percent/ApplicationTests.java
@@ -0,0 +1,13 @@
+package percent.percent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
