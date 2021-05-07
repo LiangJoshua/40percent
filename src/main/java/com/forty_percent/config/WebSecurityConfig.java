53fda6136486d9633abdc6c70777f50cfb2c8141
ruin <ruin@zillowgroup.com>
2021-05-06 23:11:49 -0700

    basic user auth based on Spring boot security

---
+++ src/main/java/com/forty_percent/config/WebSecurityConfig.java
@@ -0,0 +1,71 @@
+package com.forty_percent.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.forty_percent.service.ApplicationUserService;
+
+@Configuration
+@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final PasswordEncoder passwordEncoder;
+	private final ApplicationUserService applicationUserService;
+
+	@Autowired
+	public WebSecurityConfig(
+			PasswordEncoder passwordEncoder,
+			ApplicationUserService applicationUserService){
+		this.passwordEncoder = passwordEncoder;
+		this.applicationUserService = applicationUserService;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http
+//				.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//				.and()
+				.csrf().disable()
+				.authorizeRequests()
+					.antMatchers("/", "index", "/register**", "/users/register", "/css/*", "/js/*").permitAll()
+					.anyRequest()
+					.authenticated()
+				.and()
+				.formLogin()
+					.loginPage("/login").permitAll()
+					.defaultSuccessUrl("/profile", true)
+					.usernameParameter("username")
+					.passwordParameter("password")
+				.and()
+				.rememberMe()
+					.rememberMeParameter("remember-me")
+				.and()
+				.logout()
+					.logoutUrl("/logout")
+					.clearAuthentication(true)
+					.invalidateHttpSession(true)
+					.deleteCookies("JSESSIONID", "remember-me")
+					.logoutSuccessUrl("/login");
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.authenticationProvider(daoAuthenticationProvider());
+	}
+
+	@Bean
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setPasswordEncoder(passwordEncoder);
+		provider.setUserDetailsService(applicationUserService);
+		return provider;
+	}
+}
--- src/main/java/com/forty_percent/entity/ApplicationUser.java
+++ src/main/java/com/forty_percent/entity/ApplicationUser.java
@@ -1,18 +1,32 @@
 package com.forty_percent.entity;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.forty_percent.config.UserRole;
+
 @Entity
 @Table (indexes = { @Index (columnList = "username"), @Index(columnList = "email") })
-public class ApplicationUser implements Serializable{
+public class ApplicationUser implements UserDetails, Serializable{
 	@Id
 	@GeneratedValue (strategy= GenerationType.IDENTITY)
 	private Long id;
@@ -22,52 +36,118 @@ public class ApplicationUser implements Serializable{
 	private String username;
 	@Column (unique=true)
 	private String email;
-	private String passwordEncoded;
+	private String password;
+	@Enumerated (EnumType.STRING)
+	@ElementCollection(fetch = FetchType.EAGER)
+	private List<UserRole> roles;
+	private boolean isAccountNonExpired;
+	private boolean isAccountNonLocked;
+	private boolean isCredentialsNonExpired;
+	private boolean isEnabled;
 
 	public ApplicationUser(){
 	}
 
-	public String getEmail(){
-		return email;
-	}
-
-	public void setEmail(String email){
+	public ApplicationUser(Long id, String firstName, String lastName, String username, String email,
+			String password, List<UserRole> roles, boolean isAccountNonExpired,
+			boolean isAccountNonLocked, boolean isCredentialsNonExpired, boolean isEnabled){
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
 		this.email = email;
+		this.password = password;
+		this.roles = roles;
+		this.isAccountNonExpired = isAccountNonExpired;
+		this.isAccountNonLocked = isAccountNonLocked;
+		this.isCredentialsNonExpired = isCredentialsNonExpired;
+		this.isEnabled = isEnabled;
 	}
 
-	public Long getId(){
-		return id;
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities(){
+		return roles.stream().map(UserRole::getGrantedAuthorities).flatMap(Set::stream).collect(Collectors.toList());
+	}
+
+	@Override
+	public String getPassword(){
+		return password;
+	}
+
+	@Override
+	public String getUsername(){
+		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired(){
+		return isAccountNonExpired;
+	}
+
+	@Override
+	public boolean isAccountNonLocked(){
+		return isAccountNonLocked;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired(){
+		return isCredentialsNonExpired;
+	}
+
+	@Override
+	public boolean isEnabled(){
+		return isEnabled;
 	}
 
 	public String getFirstName(){
 		return firstName;
 	}
 
-	public void setFirstName(String firstName){
-		this.firstName = firstName;
-	}
-
 	public String getLastName(){
 		return lastName;
+	}
+
+	public String getEmail(){
+		return email;
+	}
+
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
 	}
 
 	public void setLastName(String lastName){
 		this.lastName = lastName;
 	}
 
-	public String getUsername(){
-		return username;
-	}
-
 	public void setUsername(String username){
 		this.username = username;
 	}
 
-	public String getPasswordEncoded(){
-		return passwordEncoded;
+	public void setEmail(String email){
+		this.email = email;
 	}
 
-	public void setPasswordEncoded(String passwordEncoded){
-		this.passwordEncoded = passwordEncoded;
+	public void setPassword(String password){
+		this.password = password;
+	}
+
+	public void setRoles(List<UserRole> roles){
+		this.roles = roles;
+	}
+
+	public void setAccountNonExpired(boolean accountNonExpired){
+		isAccountNonExpired = accountNonExpired;
+	}
+
+	public void setAccountNonLocked(boolean accountNonLocked){
+		isAccountNonLocked = accountNonLocked;
+	}
+
+	public void setCredentialsNonExpired(boolean credentialsNonExpired){
+		isCredentialsNonExpired = credentialsNonExpired;
+	}
+
+	public void setEnabled(boolean enabled){
+		isEnabled = enabled;
 	}
 }
